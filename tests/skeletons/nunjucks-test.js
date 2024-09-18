6dce934f229285eac03b2adb24ea83d84b3e9b98
Christopher Engleby <cengleby86@gmail.com>
2024-09-18 12:51:48 -0400

    assert test cases

--- tests/skeletons/nunjucks-test.js
+++ tests/skeletons/nunjucks-test.js
@@ -2,6 +2,7 @@ const nunjucks = require('nunjucks');
 const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
+const assert = require('assert');
 
 nunjucks.configure({ autoescape: true });
 
@@ -18,4 +19,14 @@ template = template.replace(/\${{/g, '{{');
 // Render the template with the YAML content
 const renderedOutput = nunjucks.renderString(template, yamlContent);
 
-console.log(renderedOutput)
+// Assert the rendered output is equal to the expected result
+try {
+    assert.strictEqual(
+        renderedOutput, 
+        yamlContent["expected"], 
+        'Rendered output does not match expected result.'
+    );
+    console.log(`Test ${yamlFile}: Passed`);
+} catch (error) {
+    console.error(`Test ${yamlFile}: Failed - Does not match expected`);
+}
